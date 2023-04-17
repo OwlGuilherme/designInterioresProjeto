b0fc3470babaeb68fa4d28089148a2760427f12b
jessicaduarte95 <jessicaduarte.95@gmail.com>
2023-04-17 11:40:50 -0300

    Parte inicial do site

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,34 @@
+import './index.css'
+
+export const Home = () => {
+
+  return(
+    <div className='container'>
+      <div className='pageHome'>
+        <header>
+          <div className='sectionTop'>
+            <div>
+              <p className='title'>
+                Design de Interiores
+              </p>
+            </div>
+            <nav>
+              <ul>
+                <a href="#">Início</a>
+                <a href="#">Sobre</a>
+                <a href="#">Serviços</a>
+                <a href="#">Contato</a>
+              </ul>
+            </nav>
+          </div>
+        </header>
+        <div className='sectionCenter'>
+          <div className='line1'></div>
+          <p className='name'>Soraia Duarte</p>
+          <p className='design'>Design de Interiores</p>
+          <div className='line2'></div>
+        </div>
+      </div>
+    </div>
+  )
+}
