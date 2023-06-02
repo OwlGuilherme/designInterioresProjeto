059dbaa36bcff691204836fd600eb83722cf934a
jessicaduarte95 <jessicaduarte.95@gmail.com>
2023-06-02 14:51:56 -0300

    Melhoria na responsividade do site

--- src/pages/Contatos/index.jsx
+++ src/pages/Contatos/index.jsx
@@ -6,8 +6,8 @@ export const Contatos = () => {
             <div className='pageContatos'>
                 <div className='contatos'>
                     <p className='contatosName'>Contatos</p>
-                    <p>Telefone: (61) 98516-8437</p>
-                    <p>Email: soraiaduartte@gmail.com</p>
+                    <p className='contatosTelEmail'>Telefone: (61) 98516-8437</p>
+                    <p className='contatosTelEmail'>Email: soraiaduartte@gmail.com</p>
                     <div className='lineContatos'></div>
                 </div>
             </div>
