664c3c60ed59e168d7ce8884689679179654bcd5
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-06-03 20:08:49 -0300

    Transformado o email em um link de email

--- src/pages/Contatos/index.jsx
+++ src/pages/Contatos/index.jsx
@@ -7,7 +7,7 @@ export const Contatos = () => {
                 <div className='contatos'>
                     <p className='contatosName'>Contatos</p>
                     <p className='contatosTelEmail'>Telefone: (61) 98516-8437</p>
-                    <p className='contatosTelEmail'>Email: soraiaduartte@gmail.com</p>
+                    <p className='contatosTelEmail'>Email: <a href="mailto:soraiaduartte@gmail.com">soraiaduartte@gmail.com</a></p>
                     <div className='lineContatos'></div>
                 </div>
             </div>
